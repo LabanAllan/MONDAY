3010939b816e7161dfd934c2a3885e795624a909
Laban Allan <labanallan254@gmail.com>
2022-08-26 21:31:43 +0300

    my first commit

---
+++ nav.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var x=documet.getElementById("myTopnav");
+            if(x.className=== "topnav"){
+                x.className +="responsive";
+            }
+                else{
+                    x.className="topnav";
+                }
+}
